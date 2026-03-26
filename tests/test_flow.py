5b24b6c56eb4b5b653ddc1a5dce130faad917a2e
˶𝞢⤬⫒ⵖsᐼ˶ <91800957+exdysa@users.noreply.github.com>
2026-03-25 20:41:37 -0400

    ~pin dep version

--- tests/test_flow.py
+++ tests/test_flow.py
@@ -1,5 +1,6 @@
-#  # # <!-- // /*  SPDX-License-Identifier: MPL-2.0*/ -->
-#  # # <!-- // /*  d a r k s h a p e s */ -->
+# SPDX-License-Identifier: MPL-2.0 AND LicenseRef-Commons-Clause-License-Condition-1.0
+# <!-- // /*  d a r k s h a p e s */ -->
+
 import pytest
 from unittest import TestCase
 from nnll.monitor.console import nfo
