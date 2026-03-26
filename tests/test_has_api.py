2e7552ccfa84658febd45ebc8f6018acf0145b3c
˶𝞢⤬⫒ⵖsᐼ˶ <91800957+exdysa@users.noreply.github.com>
2026-03-25 20:12:49 -0400

    ~patch test headers

--- tests/test_has_api.py
+++ tests/test_has_api.py
@@ -1,3 +1,6 @@
+# SPDX-License-Identifier: MPL-2.0 AND LicenseRef-Commons-Clause-License-Condition-1.0
+# <!-- // /*  d a r k s h a p e s */ -->
+
 import pytest
 from unittest.mock import patch, MagicMock
 import requests
